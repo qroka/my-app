c98cc6530fa5c8745bad1c999d7d6360825832c0
qroka <kolomiets@sokral.ru>
2025-09-04 16:56:15 +0500

    fix bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,18 @@
+import DarkVeil from '../components/DarkVeil';
 import { Navbar, NavbarItem, NavbarSection } from '@/components/navbar';
 
 export default function Home() {
   return (
-    <main
-      className="
-        full-viewport safe-pads
-        relative overflow-hidden
-        bg-[url('/bg.jpg')] bg-cover bg-center
-      "
-    >
-      {/* Можно: bg-[length:120%] или bg-[size:cover] под задачу */}
-      {/* Контент поверх фона */}
-      <div className="absolute inset-0 bg-black/30" />
-      <section className="relative z-10 flex h-full items-center justify-center">
-        {/* ... */}
-      </section>
-      <Navbar>
+    <div style={{ width: '100%', height: '100dvh', position: 'relative'}}>
+    <DarkVeil
+    />
+    <Navbar>
       <NavbarSection>
         <NavbarItem href="/">Home</NavbarItem>
         <NavbarItem href="/">Docs</NavbarItem>
       </NavbarSection>
     </Navbar>
-    </main>
+  </div>
+  
   );
 }
