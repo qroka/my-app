cb6ce734d44bb43c3ce16d6f0ae60221f342c698
qroka <kolomiets@sokral.ru>
2025-09-04 16:19:34 +0500

    bg fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,10 @@
-import Dither from './Dither';
+import DarkVeil from '../components/DarkVeil';
 import { Navbar, NavbarItem, NavbarSection } from '@/components/navbar';
 
 export default function Home() {
   return (
     <div style={{ width: '100%', height: '100dvh', position: 'relative'}}>
-     <Dither
-      waveColor={[0.5, 0.5, 0.5]}
-      disableAnimation={false}
-      enableMouseInteraction={true}
-      mouseRadius={0.1}
-      colorNum={4}
-      waveAmplitude={0.3}
-      waveFrequency={3}
-      waveSpeed={0.05}
+    <DarkVeil
     />
     <Navbar>
       <NavbarSection>
